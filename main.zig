9fd7961acc2e2c685992c9520cfc5e09d0fb2801
Tengs Penkwe <tengspenkwe@gmail.com>
2023-04-27 00:38:31 -0700

    use readline instead of my own commandline history

--- main.zig
+++ main.zig
@@ -8,68 +8,40 @@ const rdl = @cImport({
     @cInclude("stdio.h");
     @cInclude("stdlib.h");
     @cInclude("readline/readline.h");
-    // @cInclude("/opt/homebrew/opt/readline/include/readline/history.h");
+    @cInclude("readline/history.h");
 });
 
-const Inputs = struct {
-    commands: std.ArrayList([]const u8),
-
-    pub fn read_input(self: *@This(), allocator: std.mem.Allocator) !void {
-        var buffer: [1024]u8 = undefined;
-        const command = try stdin.readUntilDelimiterOrEof(&buffer, '\n');
-
-        if ((command == null) or !valid(command.?)) {
-            return;
-        }
-
-        const copied_command = try allocator.dupe(u8, command.?);
-        try self.commands.append(copied_command);
-
-        // std.debug.print("{s}\n", .{copied_command});
-        // self.execute(copied_command);
-    }
-
-    // fn execute(self: *@This(), command: []u8) !void {
-    //     //TODO: support up, down keys to lookthrough history
-    //     _ = self;
-    //     if (command == ".exit") {
-    //         std.os.exit(0);
-    //     } else {
-    //         stdout.print("Unrecognized command '{s}'.\n", .{command}) catch {};
-    //     }
-    // }
-
-    fn valid(command: []u8) bool {
-        //TODO: test if the command is valid;
-        _ = command;
-        return true;
-    }
-};
-
-pub fn print_prompt() !void {
-    _ = try stdout.write("db > ");
-}
 
 pub fn main() !void {
     //Allocator
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     const allocator = gpa.allocator();
+    _ = allocator;
     defer {
         //TODO: deal with it
         _ = gpa.deinit();
     }
 
-    // const result = rdl.readline(">>> ");
-    // _ = result;
+    // Configure readline to auto-complete paths when the tab key is hit.
+    _ = rdl.rl_bind_key('\t', rdl.rl_complete);
+    // Enable history
+    _ = rdl.using_history();
 
-    _ = rdl.printf("test");
-
-    var inputs = Inputs{
-        .commands = std.ArrayList([]const u8).init(allocator),
-    };
+    // Completion
+    rdl.rl_attempted_completion_function = 
 
     while (true) {
-        try print_prompt();
-        try inputs.read_input(allocator);
+      const line = rdl.readline("db > ");
+      defer rdl.free(line);
+
+      if (line == null) {
+        break;
+      }
+
+      rdl.add_history(line);
+
+      std.debug.print("\t here:{s}\n", .{line});
+
+      // try inputs.read_input(allocator);
     }
 }
